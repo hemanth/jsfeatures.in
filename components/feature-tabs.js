b854956365f0a618f2b721ef9a45ccd282303e12
Hemanth.HM <hemanth.hm@gmail.com>
2015-08-01 21:42:09 +0530

    Fixed runner gif image.

--- components/feature-tabs.js
+++ components/feature-tabs.js
@@ -16,12 +16,12 @@ class FeatureTabs extends BaseComponent {
             <Tab label="ES7/ES2016">
                 <img src="http://33.media.tumblr.com/5e0cc26f0166d9fae90c6e015582117a/tumblr_n7w5d0l8Cc1qiywueo3_500.gif"
                     alt="Backyard Banditry"
-                    style={{ position: 'relative',margin: 'auto',top: 33,left: 0,right: 0,bottom: 0}}/>
+                    style={{ position: 'relative',margin: 'auto',top: 3,left: 0,right: 0,bottom: 0}}/>
             </Tab>
             <Tab label="ES5">
                 <img src="http://33.media.tumblr.com/5e0cc26f0166d9fae90c6e015582117a/tumblr_n7w5d0l8Cc1qiywueo3_500.gif"
                     alt="Backyard Banditry"
-                    style={{ position: 'relative',margin: 'auto',top: 33,left: 0,right: 0,bottom: 0}}/>
+                    style={{ position: 'relative',margin: 'auto',top: 3,left: 0,right: 0,bottom: 0}}/>
             </Tab>
         </Tabs>
     </div>
