545a778276ccde36b61ca1e3b1eb257f04de2389
Stoeffel <christoph.hermann@kaba.com>
2015-08-15 15:11:27 +0200

    listen to hashchange

--- components/feature-tabs.js
+++ components/feature-tabs.js
@@ -5,15 +5,34 @@ import mui, { Tab, Tabs } from 'material-ui';
 
 class FeatureTabs extends BaseComponent {
   constructor () {
-    super()
+    super();
+
+
+    this.state = {
+        tabIndex: this.getTabIndex()
+    };
+
+    window.addEventListener("hashchange", this.onHashChange.bind(this), false);
   }
-  render () {
+
+  onHashChange () {
+    this.setState({
+        tabIndex: this.getTabIndex()
+    });
+  }
+
+  getTabIndex () {
     // Till material-ui gives an option to update tabIndex programatically.
     const tabIndex = (window.location.href.indexOf("#ES7")>=0 ? 0 :
                       window.location.href.indexOf("#ES6")>=0 ? 1 :
                       window.location.href.indexOf("#ES5")>=0 ? 2 : 0);
+    return tabIndex;
+  }
+
+  render () {
+
     return (<div style={{'maxWidth': '1024', 'margin':'auto'}}>
-        <Tabs initialSelectedIndex={tabIndex}>
+        <Tabs initialSelectedIndex={this.state.tabIndex}>
             <Tab label="ES7/ES2016" id="es7" onActive={() => window.location = '#ES7'}>
                 <Feature spec='ES7' />
             </Tab>
