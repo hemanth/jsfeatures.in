edea7a980d64f5320b5f6646087b078ee9686781
Hemanth.HM <hemanth.hm@gmail.com>
2016-01-25 19:57:19 +0530

    Lighter build

--- build.bash
+++ build.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 echo "Building..."
 NODE_ENV=production
-browserify app/main.js | uglifyjs -cm > public/bundle.js
+rm -rf public
+mkdir public
+browserify app/main.js app/scripts/main.js | uglifyjs -cm > public/bundle.js
 cp app/index.html public/index.html
 cp app/manifest.json public/mainfest.json
 cp app/manifest.webapp public/mainfest.webapp
 cp app/service-worker.js public/service-worker.js
 cp app/sitemap.xml public/sitemap.xml
-cp -r app/scripts public/scripts
 echo "Done!"
