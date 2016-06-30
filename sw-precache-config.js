06603c6ee7f46951ea3f78c3dcc80afe7e6fe706
Hemanth.HM <hemanth.hm@gmail.com>
2016-06-30 20:16:08 +0530

    sw-precache-config

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,17 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/prims/themes/prism.css',
+    '/bower_components/prims/prism.js',
+    '/bower_components/prism-element/prism-import.html',
+    '/src/data/es5.md',
+    '/src/data/es6.md',
+    '/src/data/es7.md',
+    '/src/js-features.html'
+    '/src/es5-features.html'
+    '/src/es6-features.html'
+    '/src/es7-features.html'
   ],
   navigateFallback: '/index.html'
 };
